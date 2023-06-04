d2f9e2bbbed99899e032c2c4a7ecbc1bece4c8e3
Mukuakuiza <42307213+Mukuakuiza@users.noreply.github.com>
2023-06-04 22:50:34 +0200

    EJs mate installation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const ejsMate =require('ejs-mate');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const path = require('path')
@@ -13,10 +14,9 @@ async function main() {
   console.log("Mongoose connection open!!");
 }
 
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+app.engine('ejs', ejsMate);
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))//used to override method get or post when submit the edit form
 
