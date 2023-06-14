2be99293d99615c86509804b1e7c7b7f726fc606
Mukuakuiza <42307213+Mukuakuiza@users.noreply.github.com>
2023-06-15 01:05:54 +0200

    created the review, where it submits

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ExpressError = require('./utils/expressError')
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const path = require('path')
+const Review = require('./models/review')
 
 main().catch(err => console.log(err));
 
@@ -82,6 +83,16 @@ app.delete('/campgrounds/:id',catchAsync( async(req, res)=>{
   const {id} = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds')
+}))
+
+//review
+app.post('/campgrounds/:id/reviews', catchAsync(async(req,res)=>{
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review (req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.all('*', (req,res,next)=>{
