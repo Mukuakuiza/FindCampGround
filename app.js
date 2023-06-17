d248f888f0c061d3283f9dfabe33db6864b82368
Mukuakuiza <42307213+Mukuakuiza@users.noreply.github.com>
2023-06-18 00:45:30 +0200

    added validating reviews schema

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const ejsMate =require('ejs-mate');
-const {campgroundSchema} = require('./schemas.js')
+const {campgroundSchema, reviewSchema} = require('./schemas.js')
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/expressError')
 const Campground = require('./models/campground');
@@ -33,6 +33,16 @@ const validateCampground = (req, res, next)=>{
     }else{
       next()
     }
+}
+
+const validateReview =(req,res,next)=>{
+  const {error} = reviewSchema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  }else{
+    next()
+  }
 }
 
 app.get('/', (req,res)=>{
@@ -86,7 +96,7 @@ app.delete('/campgrounds/:id',catchAsync( async(req, res)=>{
 }))
 
 //review
-app.post('/campgrounds/:id/reviews', catchAsync(async(req,res)=>{
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req,res)=>{
   const campground = await Campground.findById(req.params.id);
   const review = new Review (req.body.review);
   campground.reviews.push(review);
