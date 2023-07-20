04f3df6ff6a7c9eedd20aff70be03d1277843089
Mukuakuiza <42307213+Mukuakuiza@users.noreply.github.com>
2023-07-20 23:28:30 +0200

    created registration basic form

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user')
+
+//register -form show
+router.get('/register', (req,res)=>{
+    res.render('users/register')
+})
+
+//post register- create a user(where the register form submits)
+router.post('/register', catchAsync( async(req, res)=>{
+    try{
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yatena Camp!');
+        res.redirect('/campgrounds')
+    }catch(e){
+        req.flash('error', e.message)
+        res.redirect('register')
+    }
+   
+}))
+
+module.exports = router;
