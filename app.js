9a2c2a0a1e2d8be84c953ca8b306c1eb8b08bdc2
Mukuakuiza <42307213+Mukuakuiza@users.noreply.github.com>
2023-10-22 18:09:03 +0200

    hiding erros

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(mongoSanitize())
 
 const sessionConfig={
+  name: 'session',
   secret: 'thisshouldbebettersecret!',
   resave: false,
   saveUninitialized: true,
